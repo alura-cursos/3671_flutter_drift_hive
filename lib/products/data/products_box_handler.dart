4b0ffcd855ff5230970b5c7b9788dea66467df7b
Ricarth Lima <ricarth.lima@gmail.com>
2024-02-20 14:23:29 -0300

    v4.4 - usando objetos personalizados

--- lib/products/data/products_box_handler.dart
+++ lib/products/data/products_box_handler.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_listin/products/model/product.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class ProductsBoxHandler {
@@ -10,4 +11,6 @@ class ProductsBoxHandler {
   Future<void> closeBox() async {
     return _box.close();
   }
+
+  insertProduct(Product product) {}
 }
